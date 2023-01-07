7a893cd8eb1043fda41bab6fc8151b7dfd0bd270
mike wakerly <opensource@hoho.com>
2023-01-07 11:30:27 -0800

    app: add basic scaffolding
    - react-navigation for nav
    - react-native-paper for misc ui
    - safe-area-provider... for that stuff?

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+// TODO(mikey): Use our very own special colors here.
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider
+      theme={theme}
+      settings={{
+        icon: (props) => <FontAwesome5 {...props} />,
+      }}
+    >
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="auto" />
+          </View>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
 
